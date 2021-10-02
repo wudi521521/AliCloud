ecf29cf1df720ed3ebe28e6c592d9ce1007e6ee6
Lenovo <781311383@qq.com>
2021-10-02 19:55:34 +0800

    nacos config 空间，分组

--- ali-parent/ali-order/src/main/java/com/cloud/OrderApplication.java
+++ ali-parent/ali-order/src/main/java/com/cloud/OrderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,7 +20,18 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 public class OrderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OrderApplication.class);
+        //SpringApplication.run(OrderApplication.class);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(OrderApplication.class,args);
+        while(true){
+            String userName = applicationContext.getEnvironment().getProperty("user.name");
+            System.out.println("user_name:"+userName);
+            try{Thread.sleep(1900);}catch (Exception e){
+             //nacos客户端每10ms去注册中心进行判断,根据MD5进行判断
+            }
+
+        }
+
+
     }
 
    /* @Bean
@@ -33,4 +45,6 @@ public class OrderApplication {
    public RestTemplate getRestTemplate(){
        return new RestTemplate();
    }
+
+
 }
